49f5edc89758f20c31bfd2831cde04fa14d7a965
IB_Student <student@InnovatBhamsMBP.attlocal.net>
2022-03-02 20:39:19 -0600

    template literal and default parameter values

---
+++ app.js
@@ -0,0 +1,13 @@
+    //Template Literals and Default Parameter Values
+//function favMovie(name="Tommy Wiseau",movie="the room"){
+//    console.log(`my name is ${name} and my favorite movie is ${movie}`); 
+//}
+//favMovie("The Karate Kid");
+//favMovie();
+//favMovie("Daniel", "Avatar");
+    //Arrow Functions
+let favMovie = (name="Tommy Wiseau",movie="the room")=> console.log(`my name is ${name} and my favorite movie is ${movie}`); 
+favMovie();
+
+
+ 
