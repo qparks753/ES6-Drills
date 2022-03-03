64a712c7c1f6589919fd3c2881ba1b104f61480e
IB_Student <student@InnovatBhamsMBP.attlocal.net>
2022-03-02 21:12:09 -0600

    added arrow function section

--- app.js
+++ app.js
@@ -8,6 +8,16 @@
     //Arrow Functions
 let favMovie = (name="Tommy Wiseau",movie="the room")=> console.log(`my name is ${name} and my favorite movie is ${movie}`); 
 favMovie();
+let getFirstName = (str)=> {
+    let split = str.split(" "); return split[0]
+};
+console.log(getFirstName("Joan Davis"));
+let getFirstNameConcise = (str) => {let split = str.split(" "); return split[0]};
+console.log(getFirstNameConcise("David Johnson"));
 
+let doMath = {exponent:(x,y)=>{return x**y}, multiply:(x,y)=>{return x*y} };
+//console.log(doMath.exponent(3,4));
+//console.log(doMath.multiply(3,4));
+console.log(`${doMath.exponent(3,4)} ${doMath.multiply(3,4)}`);
 
  
